564a7508d1272bb1cc6c8dc0d345dee0467a3b59
Soumyadeep Biswas <soumyadeep.bs2023@gmail.com>
2025-09-10 23:19:37 +0530

    Added few Important Components

---
+++ src/components/PostList.jsx
@@ -0,0 +1,10 @@
+import { Post } from "./Post"
+export const PostList =()=>{
+  return(
+    <>
+    <Post></Post>
+    <Post></Post>
+    <Post></Post> 
+    </>
+  )
+}
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,14 +4,11 @@ const Sidebar = () =>{
    <ul class="nav nav-pills flex-column mb-auto"> <li class="nav-item"> <a href="#" class="nav-link active" aria-current="page"> <svg class="bi pe-none me-2" width="16" height="16" aria-hidden="true"><use xlinkHref="#home"></use></svg>
 Home
 </a> </li> <li> <a href="#" class="nav-link text-white"> <svg class="bi pe-none me-2" width="16" height="16" aria-hidden="true"><use xlinkHref="#speedometer2"></use></svg>
-Dashboard
-</a> </li> <li> <a href="#" class="nav-link text-white"> <svg class="bi pe-none me-2" width="16" height="16" aria-hidden="true"><use xlinkHref="#table"></use></svg>
-Orders
-</a> </li> <li> <a href="#" class="nav-link text-white"> <svg class="bi pe-none me-2" width="16" height="16" aria-hidden="true"><use xlinkHref="#grid"></use></svg>
-Products
-</a> </li> <li> <a href="#" class="nav-link text-white"> <svg class="bi pe-none me-2" width="16" height="16" aria-hidden="true"><use xlinkHref="#people-circle"></use></svg>
-Customers
-</a> </li> </ul> <hr/> <div class="dropdown"> <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false"> <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2"/> <strong>mdo</strong> </a> <ul class="dropdown-menu dropdown-menu-dark text-small shadow"> <li><a class="dropdown-item" href="#">New project...</a></li> <li><a class="dropdown-item" href="#">Settings</a></li> <li><a class="dropdown-item" href="#">Profile</a></li> <li><hr class="dropdown-divider"/></li> <li><a class="dropdown-item" href="#">Sign out</a></li> </ul> </div> </div>
+Create Post
+    </a> 
+  </li> 
+ </ul>
+  <hr/> <div class="dropdown"> <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false"> <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2"/> <strong>mdo</strong> </a> <ul class="dropdown-menu dropdown-menu-dark text-small shadow"> <li><a class="dropdown-item" href="#">New project...</a></li> <li><a class="dropdown-item" href="#">Settings</a></li> <li><a class="dropdown-item" href="#">Profile</a></li> <li><hr class="dropdown-divider"/></li> <li><a class="dropdown-item" href="#">Sign out</a></li> </ul> </div> </div>
     
 );
 }
