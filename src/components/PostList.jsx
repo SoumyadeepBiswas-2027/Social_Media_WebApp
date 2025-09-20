0154e6893a96e439a2201918b59d74554e1c3c4d
Soumyadeep Biswas <soumyadeep.bs2023@gmail.com>
2025-09-21 00:02:03 +0530

    Update PostList component

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,26 +1,23 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import { Post } from "./Post";
 import { PostList as PostListData } from "../store/post-list-store";
 import { WelcomeMessage } from "./WelcomeMessage";
+import { useEffect } from "react";
 export const PostList = () => {
   const { postList, addInitialPosts } = useContext(PostListData);
-  const [DataFetched, setDataFetched] = useState(false);
+ // const [DataFetched, setDataFetched] = useState(false);
 
-  if (!DataFetched) {
+  useEffect(() =>{
     fetch("https://dummyjson.com/posts")
       .then((res) => res.json())
       .then((data) => {
         addInitialPosts(data.posts);
       });
-    setDataFetched(true);
-  }
+  }, []);
+  
 
   const handleGetPostsClick = () => {
-    // fetch("https://dummyjson.com/posts")
-    //   .then((res) => res.json())
-    //   .then((data) => {
-    //     addInitialPosts(data.posts);
-    //   });
+   
   };
 
   return (
