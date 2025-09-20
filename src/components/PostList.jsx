7dd7914fc33e4241d3f2ed49d1b068900a8e587f
Soumyadeep Biswas <soumyadeep.bs2023@gmail.com>
2025-09-20 22:11:39 +0530

    Fetched data while loading ,not be clciking a button and used useaState hook for it

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,24 +1,31 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Post } from "./Post";
 import { PostList as PostListData } from "../store/post-list-store";
 import { WelcomeMessage } from "./WelcomeMessage";
 export const PostList = () => {
   const { postList, addInitialPosts } = useContext(PostListData);
+  const [DataFetched, setDataFetched] = useState(false);
 
-  const handleGetPostsClick = () => {
-
+  if (!DataFetched) {
     fetch("https://dummyjson.com/posts")
       .then((res) => res.json())
-      .then(data =>{
+      .then((data) => {
         addInitialPosts(data.posts);
       });
+    setDataFetched(true);
+  }
+
+  const handleGetPostsClick = () => {
+    // fetch("https://dummyjson.com/posts")
+    //   .then((res) => res.json())
+    //   .then((data) => {
+    //     addInitialPosts(data.posts);
+    //   });
   };
 
   return (
     <>
-      {postList.length === 0 && (
-        <WelcomeMessage onGetPostsClick={handleGetPostsClick} />
-      )}
+      {postList.length === 0 && <WelcomeMessage />}
 
       {postList.map((post) => (
         <Post key={post.Id} post={post} />
