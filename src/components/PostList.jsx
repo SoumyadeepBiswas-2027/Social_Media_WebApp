4b6a496c08a6d038b330ac04f6a99049d8502d00
Soumyadeep Biswas <soumyadeep.bs2023@gmail.com>
2025-09-24 23:59:07 +0530

    Adv UseEffect concept and cleanup added

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -10,19 +10,24 @@ export const PostList = () => {
  const[fetching ,setFetching ]=useState(false)
 
 
-
-
   useEffect(() =>{
     setFetching(true);
-    fetch("https://dummyjson.com/posts")
+    const controller = new AbortController();
+    const signal = controller.signal;
+
+    fetch("https://dummyjson.com/posts",{signal})
       .then((res) => res.json())
       .then((data) => {
         addInitialPosts(data.posts);
          setFetching(false);
       });
+
+      return () => {
+        console.log("cleaning up UseEffect");
+        controller.abort();
+      }
   }, []);
   
-
   const handleGetPostsClick = () => {
    
   };
